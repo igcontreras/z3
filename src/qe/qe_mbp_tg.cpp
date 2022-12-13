ec071a3eedf963eb3f0ce7b943681c80b07836cf
hgvk94 <hgvk94@gmail.com>
2022-12-13 00:11:30 -0500

    rewrite

--- src/qe/qe_mbp_tg.cpp
+++ src/qe/qe_mbp_tg.cpp
@@ -64,45 +64,167 @@ private:
   bool has_arr_var(expr* t) {
     return contains_vars(t, m_vars);
   }
+  bool is_arr_var(expr* t) {
+    return is_uninterp_const(t) && has_arr_var(t);
+  }
+
+  bool is_wr_on_rhs(expr* e) {
+    return is_app(e) && is_partial_eq(to_app(e)) && is_wr_on_rhs(to_app(e)->get_arg(0), to_app(e)->get_arg(1));
+  }
+
+  bool is_wr_on_rhs(expr* lhs, expr* rhs) {
+    return (is_arr_write(rhs) && !is_arr_write(lhs));
+  }
+
+  bool should_create_peq(expr* e) {
+    return m.is_eq(e) && should_create_peq(to_app(e)->get_arg(0), to_app(e)->get_arg(1));
+  }
+
+  bool should_create_peq(expr* lhs, expr* rhs) {
+    return m_array_util.is_array(lhs) && m_array_util.is_array(rhs) && (has_arr_var(lhs) || has_arr_var(rhs));
+  }
+
+  peq mk_peq(expr* e1, expr* e2) {
+    vector<expr_ref_vector> empty;
+    return mk_peq(e1, e2, empty);
+  }
+
+  peq mk_peq(expr* e1, expr* e2, vector<expr_ref_vector>& indices) {
+    expr* n_lhs, *n_rhs;
+    if (is_wr_on_rhs(e1, e2)) {
+	n_lhs = e2;
+	n_rhs = e1;
+      }
+      else {
+	n_lhs = e1;
+	n_rhs = e2;
+      }
+      return peq(n_lhs, n_rhs, indices, m);
+  }
   
   void preprocess(expr_ref_vector& fml) {
     int j = 0;
     vector<expr_ref_vector> empty;
     for(expr* e : fml) {
-      if (m.is_eq(e) && m_array_util.is_array(to_app(e)->get_arg(0)) && (has_arr_var(to_app(e)->get_arg(0)) || has_arr_var(to_app(e)->get_arg(1)))) {
-        peq pe(to_app(e)->get_arg(1), to_app(e)->get_arg(0), empty, m);
-	fml[j] = pe.mk_peq();
-      }
-      j++;
-    }
-    j = 0;
-    for(expr* e : fml) {
-      if (is_app(e) && is_partial_eq(to_app(e)) && (is_arr_write(to_app(e)->get_arg(1)) && !is_arr_write(to_app(e)->get_arg(0)))) {
-	peq ex(to_app(e), m);
-	vector<expr_ref_vector> result;
-	ex.get_diff_indices(result);
-	peq pe(ex.rhs(), ex.lhs(), result, m);
-	fml[j] = pe.mk_peq();
+      if (should_create_peq(e)) {
+	fml[j] = mk_peq(to_app(e)->get_arg(0), to_app(e)->get_arg(1)).mk_peq();
       }
       j++;
     }
   }
+
+  void elimwreq(peq p, mbp::term_graph &tg, model& mdl, bool is_neg, expr_ref_vector& todo) {
+    SASSERT(is_arr_write(p.lhs()));
+    TRACE("mbp_tg", tout << "processing " << expr_ref(p.mk_peq(), m););
+    vector<expr_ref_vector> indices;
+    if (is_neg)
+      tg.add_lit(mk_not(p.mk_peq()));
+    else
+      tg.add_lit(p.mk_peq());
+    expr* j = to_app(p.lhs())->get_arg(1);
+    bool in = false;
+    p.get_diff_indices(indices);
+    expr* eq;
+    expr_ref_vector deq(m);
+    for(expr_ref_vector& e : indices) {
+      for (expr* i : e) {
+	if (mdl.is_true(m.mk_eq(j, i))) {
+	  in = true;
+	  eq = m.mk_eq(j, i);
+	  break;
+	}
+	else deq.push_back(i);
+      }
+    }
+    if (in) {
+      peq p_new = mk_peq(to_app(p.lhs())->get_arg(0), p.rhs(), indices);
+      tg.add_lit(p_new.mk_peq());
+      tg.add_eq(p.mk_peq(), p_new.mk_peq());
+      tg.add_lit(eq);
+      if (is_neg)
+	todo.push_back(m.mk_not(p_new.mk_peq()));
+      else
+	todo.push_back(p_new.mk_peq());
+    }
+    else {
+      for(expr* d : deq) {
+	tg.add_lit(m.mk_not(m.mk_eq(j, d)));
+      }
+      if (indices.empty()) {
+	expr_ref_vector setOne(m);
+	setOne.push_back(j);
+	indices.push_back(setOne);
+      }
+      else
+	indices.back().push_back(j);
+      peq p_new = mk_peq(to_app(p.lhs())->get_arg(0), p.rhs(), indices);
+      expr* args[2] = {p.rhs(), j};
+      expr* rd = m_array_util.mk_select(2, args);
+      eq = m.mk_eq(rd, to_app(p.lhs())->get_arg(2));
+      if (!is_neg) {
+	tg.add_lit(p_new.mk_peq());
+	tg.add_lit(eq);
+	tg.add_eq(p.mk_peq(), p_new.mk_peq());
+	todo.push_back(p_new.mk_peq());
+      }
+      else {
+	SASSERT(mdl.is_false(p_new.mk_peq()) || mdl.is_false(eq));
+	if (mdl.is_false(p_new.mk_peq()))
+	  tg.add_lit(mk_not(p_new.mk_peq()));
+	if (mdl.is_false(eq))
+	  tg.add_lit(m.mk_not(eq));
+	tg.add_eq(p.mk_peq(), p_new.mk_peq());
+	todo.push_back(mk_not(p_new.mk_peq()));
+      }
+    }
+  }
+
+  void elimeq(peq p, mbp::term_graph &tg, app_ref_vector& vars) {
+    app_ref_vector aux_consts(m);
+    expr_ref eq(m);
+    eq = p.mk_eq(aux_consts, true);
+    for(app* a : aux_consts) vars.push_back(a);
+    tg.add_lit(eq);
+    tg.add_lit(m.mk_true());
+    tg.add_eq(p.mk_peq(), m.mk_true());
+    TRACE("mbp_tg", tout << "added lit  " << eq;);
+  }
+
+  expr_ref elimrdwr(expr* term, mbp::term_graph &tg, model& mdl) {
+    SASSERT(is_rd_wr(term));
+    expr* wr_ind = to_app(to_app(term)->get_arg(0))->get_arg(1);
+    expr* rd_ind = to_app(term)->get_arg(1);
+    expr* eq = m.mk_eq(wr_ind, rd_ind);
+    expr_ref e(m);
+    if (mdl.is_true(eq)) {
+      tg.add_lit(eq);
+      e =  to_app(to_app(term)->get_arg(0))->get_arg(2);
+    }
+    else {
+      tg.add_lit(m.mk_not(eq));
+      expr* args[2] = {to_app(to_app(term)->get_arg(0))->get_arg(0), to_app(term)->get_arg(1)};
+      e = m_array_util.mk_select(2, args);
+    }
+    tg.add_lit(m.mk_eq(term, e));
+    return e;
+  }
+
 public:
   impl(ast_manager &m, params_ref const &p)
     : m(m), m_array_util(m), m_vars(m) {}
 
-  void operator()(app_ref_vector &vars, expr_ref &e, model& mdl) {
+  void operator()(app_ref_vector &vars, expr_ref &inp, model& mdl) {
     if (vars.empty())
       return;
     for(auto v : vars) m_vars.push_back(v);
     expr_ref_vector fml(m);
-    flatten_and(e, fml);
-    TRACE("mbp_tg", tout << "Before preprocess " << mk_and(fml););
-    preprocess(fml);
-    TRACE("mbp_tg", tout << "After preprocess " << mk_and(fml););
     mbp::term_graph tg(m);
     tg.set_prop_ground(true);
     tg.set_vars(vars, true /*exclude*/);    
+    flatten_and(inp, fml);
+    TRACE("mbp_tg", tout << "Before preprocess " << mk_and(fml););
+    preprocess(fml);
+    TRACE("mbp_tg", tout << "After preprocess " << mk_and(fml););
     expr_ref_vector todo(m);
     expr* f;
     for(expr *e : fml) {
@@ -110,101 +232,38 @@ public:
 	todo.push_back(e);
       }
       else if (m.is_not(e, f) && is_app(f) && is_partial_eq(to_app(f))) {
-	todo.push_back(f);
+	todo.push_back(e);
       }
       tg.add_lit(e);
     }
     vector<expr_ref_vector> indices;
-
-    //ElimWrEq begin 
     while(!todo.empty()) {
-      expr* e = todo.back();
-      todo.pop_back();
-      SASSERT(is_app(e) && is_partial_eq(to_app(e)));
-      TRACE("mbp_tg", tout << "processing " << expr_ref(e, m););
-      peq p(to_app(e), m);
-      tg.add_lit(p.mk_peq());
-      if (is_arr_write(p.lhs())) {
-	expr* j = to_app(p.lhs())->get_arg(1);
-	bool in = false;
-	indices.reset();
-	p.get_diff_indices(indices);
-	expr* eq;
-	expr_ref_vector deq(m);
-	for(expr_ref_vector& e : indices) {
-	  for (expr* i : e) {
-	    if (mdl.is_true(m.mk_eq(j, i))) {
-	      in = true;
-	      eq = m.mk_eq(j, i);
-	    }
-	    else deq.push_back(i);
-	  }
-	}
-	if (in) {
-	  peq p_new(to_app(p.lhs())->get_arg(0), p.rhs(), indices, m);
-	  tg.add_lit(p_new.mk_peq());
-	  tg.add_eq(p.mk_peq(), p_new.mk_peq());
-	  tg.add_lit(eq);
-	}
-	else {
-	  for(expr* d : deq) {
-	    tg.add_lit(m.mk_not(m.mk_eq(j, d)));
-	  }
-	  if (indices.empty()) {
-	    expr_ref_vector setOne(m);
-	    setOne.push_back(j);
-	    indices.push_back(setOne);
-	  }
-	  else
-	    indices.back().push_back(j);
-	  peq p_new(to_app(p.lhs())->get_arg(0), p.rhs(), indices, m);
-	  tg.add_lit(p_new.mk_peq());
-	  expr* args[2] = {p.rhs(), j}; 
-	  expr* rd = m_array_util.mk_select(2, args);
-	  eq = m.mk_eq(rd, to_app(p.lhs())->get_arg(2));
-	  tg.add_lit(eq);
-	  tg.add_eq(p.mk_peq(), p_new.mk_peq());
-	}
-      }
+	expr* e = todo.back();
+	todo.pop_back();
+	bool is_not = m.is_not(e, e);
+	SASSERT(is_app(e) && is_partial_eq(to_app(e)));
+	peq p(to_app(e), m);
+	if (is_arr_write(p.lhs()))
+	  elimwreq(p, tg, mdl, is_not, todo);
+	else if (is_arr_var(p.lhs()) && !contains_var(p.rhs(), app_ref(to_app(p.lhs()), m)))
+	  elimeq(p, tg, vars);
     }
-    //ElimWreq end
-
-    //ElimEq begin
-    for (expr * lit : tg.get_lits()) {
-      if (is_app(lit) && is_partial_eq(to_app(lit))) {
-	peq p(to_app(lit), m);
-	if (is_uninterp_const(p.lhs()) && has_arr_var(p.lhs()) && !contains_var(p.rhs(), app_ref(to_app(p.lhs()), m))) {
-	  app_ref_vector aux_consts(m);
-	  expr_ref eq(m);
-	  eq = p.mk_eq(aux_consts, true);
-	  for(app* a : aux_consts) m_vars.push_back(a);
-	  tg.add_vars(aux_consts);
-	  tg.add_lit(eq);
-	  tg.add_lit(m.mk_true());
-	  tg.add_eq(p.mk_peq(), m.mk_true());
-	  TRACE("mbp_tg", tout << "added lit  " << eq;);
-	}
-      }
-    }
-    //ElimEq End
-
     expr_ref_vector terms(m), rdTerms(m);
     tg.get_terms(terms);
-    for (expr* term : terms) {
+    for (unsigned i = 0; i < terms.size(); i++) {
+      expr* term = terms.get(i);
+      if (tg.is_marked(term)) continue;
+      // not used right now. Will be used when elimrdwr is applied
+      // till fp
+      tg.mark(term);
       if (is_rd_wr(term)) {
-	expr* wr_ind = to_app(to_app(term)->get_arg(0))->get_arg(1);
-	expr* rd_ind = to_app(term)->get_arg(1);
-	expr* eq = m.mk_eq(wr_ind, rd_ind);
-	if (mdl.is_true(eq)) {
-	  tg.add_lit(eq);
-	  tg.add_lit(m.mk_eq(term, to_app(to_app(term)->get_arg(0))->get_arg(2)));
-	}
-	else {
-	  tg.add_lit(m.mk_not(eq));
-	  expr* args[2] = {to_app(to_app(term)->get_arg(0))->get_arg(0), to_app(term)->get_arg(1)};
-	  expr* rdTerm = m_array_util.mk_select(2, args);
-	  tg.add_lit(m.mk_eq(term, rdTerm));
-	}
+	expr_ref e = elimrdwr(term, tg, mdl);
+	terms.push_back(e);
+	// Assuming that rdterms never return arrays, elimrdwr will
+	// not produce any new partial equalities
+	// for(expr *r : equiv_terms) {
+	// if (should_create_peq(r, e))
+	// todo.push_back(mk_peq(r, e)); }
       }
       else if (m_array_util.is_select(term) && contains_vars(to_app(term)->get_arg(0), m_vars)) {
 	rdTerms.push_back(term);
@@ -222,7 +281,7 @@ public:
 	expr* eq = m.mk_eq(i1, i2);
 	if (a1 == a2) {
 	  if (mdl.is_true(eq))
-	    tg.add_eq(i1, i2);
+	    tg.add_lit(eq);
 	  else
 	    tg.add_lit(m.mk_not(eq));
 	}
@@ -231,8 +290,8 @@ public:
 
     TRACE("mbp_tg", tout << "mbp tg " << mk_and(tg.get_lits()););
     tg.compute_non_ground<true>(m_vars);
-    e = tg.to_ground_expr();
-    TRACE("mbp_tg", tout << "after mbp tg " << e;);
+    inp = tg.to_ground_expr();
+    TRACE("mbp_tg", tout << "after mbp tg " << inp;);
   }
 
 };
