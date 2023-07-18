d51de1975a5a4968670aad3ea546251cd4adf98f
hgvk94 <hgvk94@gmail.com>
2023-07-18 18:48:53 -0400

    bug fix. vars under accessors are redundant

--- src/qe/mbp/mbp_qel_util.cpp
+++ src/qe/mbp/mbp_qel_util.cpp
@@ -61,16 +61,22 @@ struct proc {
     ast_manager &m;
     obj_hashtable<app> &m_vars;
     array_util m_array_util;
+    datatype_util m_dt_util;
     proc(obj_hashtable<app> &vars, ast_manager &man)
-        : m(man), m_vars(vars), m_array_util(m) {}
+        : m(man), m_vars(vars), m_array_util(m), m_dt_util(m) {}
     void operator()(expr *n) const {}
     void operator()(app *n) {
         if (m_array_util.is_select(n)) {
-            collect_uninterp_consts(n->get_arg(1), m_vars);
+            expr* idx = n->get_arg(1);
+            if (is_app(idx) && m_dt_util.is_accessor(to_app(idx)->get_decl())) return;
+            collect_uninterp_consts(idx, m_vars);
         }
         else if (m_array_util.is_store(n)) {
-            collect_uninterp_consts(n->get_arg(1), m_vars);
-            collect_uninterp_consts(n->get_arg(2), m_vars);
+            expr* idx = n->get_arg(1), *elem = n->get_arg(2);
+            if (!(is_app(idx) && m_dt_util.is_accessor(to_app(idx)->get_decl())))
+                collect_uninterp_consts(idx, m_vars);
+            if (!(is_app(elem) && m_dt_util.is_accessor(to_app(elem)->get_decl())))
+            collect_uninterp_consts(elem, m_vars);
         }
     }
 };
